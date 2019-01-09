9abde7ec0856a6339c6c109aadfea7c66ebb96bd
Rodolfo Viana <rodolfo@rodolfoviana.com.br>
2019-01-09 00:51:02 -0200

    Download reimbursement data

---
+++ novedejulho/gasto_cota.py
@@ -0,0 +1,34 @@
+import os
+import requests as req
+from novedejulho.toolbox import (xml_df, save_csv, save_xz)
+
+url = 'http://www.al.sp.gov.br/repositorioDados/deputados/despesas_gabinetes.xml'
+arquivo = 'gastos_cota'
+data_dir = 'data/'
+
+
+def create_dir():
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+
+
+def process_request():
+    xml_data = req.get(url).content
+    dataset = xml_df(xml_data).process_data()
+    dataset = dataset[['Deputado', 'Matricula', 'Ano', 'Mes',
+                       'Tipo', 'Fornecedor', 'CNPJ', 'Valor']]
+    dataset = dataset.rename(columns={
+        'Deputado': 'deputado', 'Matricula': 'matricula',
+        'Ano': 'ano', 'Mes': 'mes', 'Tipo': 'tipo',
+        'Fornecedor': 'fornecedor', 'CNPJ': 'cnpj', 'Valor': 'valor'
+    })
+    save_csv(dataset, data_dir, arquivo)
+    save_xz(dataset, data_dir, arquivo)
+
+
+if __name__ == '__main__':
+    print(f'Criando e acessando a pasta {data_dir}...')
+    create_dir()
+    print('Baixando e formatando os dados...')
+    process_request()
+    print(f'Finalizado!\nDados salvos nos arquivos {arquivo}.csv e {arquivo}.xz.')
