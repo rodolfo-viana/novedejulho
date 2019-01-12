9532911b0c974696601e4d7fedea8fd51ce40f97
Rodolfo Viana <rodolfo@rodolfoviana.com.br>
2019-01-12 15:46:32 -0200

    Add class to parse local xml files

--- novedejulho/toolbox.py
+++ novedejulho/toolbox.py
@@ -10,12 +10,42 @@ import xml.etree.ElementTree as ElementTree
 import pandas as pd
 
 
-# Utilitários para extrair dados de xml
+# Utilitários para extrair dados de xml online
 
 class xml_df:
 
     def __init__(self, xml_data):
         self.root = ElementTree.XML(xml_data)
+
+    def parse_root(self, root=None):
+        root = root if root else self.root
+        yield from (self.parse_element(child) for child in iter(root))
+
+    def parse_element(self, element, parsed=None):
+        if parsed is None:
+            parsed = dict()
+
+        new_values = {k: element.attrib.get(k) for k in element.keys()}
+        parsed.update(new_values)
+        if element.text:
+            parsed[element.tag] = element.text
+
+        for child in list(element):
+            self.parse_element(child, parsed)
+
+        return parsed
+
+    def process_data(self):
+        return pd.DataFrame(self.parse_root())
+
+
+# Utilitários para extrair dados de xml em arquivo local
+
+class xml_df_internal:
+
+    def __init__(self, xml_data):
+        self.tree = ElementTree.parse(xml_data)
+        self.root = self.tree.getroot()
 
     def parse_root(self, root=None):
         root = root if root else self.root
