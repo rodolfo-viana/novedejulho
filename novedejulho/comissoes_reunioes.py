2c3b7b8437dc46da017856cd2eaecc7be623c93a
Vinicius <vinicius.matheus252@gmail.com>
2019-02-23 14:31:38 -0300

    Add Reuniões de Comissão

---
+++ novedejulho/comissoes_reunioes.py
@@ -0,0 +1,29 @@
+import requests as req
+
+from ndj_toolbox.fetch import (xml_df, save_files)
+
+url_base = 'http://www.al.sp.gov.br/repositorioDados/'
+url_file = 'processo_legislativo/comissoes_permanentes_reunioes.xml'
+url = url_base + url_file
+
+
+def main():
+    xml_data = req.get(url).content
+    dataset = xml_df(xml_data).process_data()
+    dataset = dataset[[
+        'IdReuniao', 'IdComissao', 'IdPauta',
+        'NrLegislatura', 'NrConvocacao', 'TipoConvocacao',
+        'Data', 'CodSituacao', 'Situacao', 'Presidente'
+    ]]
+    dataset = dataset.rename(columns={
+        'IdReuniao': 'id_reuniao', 'IdComissao': 'id_comissao',
+        'IdPauta': 'id_pauta', 'NrLegislatura': 'nr_legislatura',
+        'NrConvocacao': 'nr_convocacao', 'TipoConvocacao': 'tp_convocacao',
+        'Data': 'dt', 'CodSituacao': 'cd_situacao',
+        'Situacao': 'situacao', 'Presidente': 'presidente'
+    })
+    save_files(dataset, 'comissoes_permanentes_reunioes')
+
+
+if __name__ == '__main__':
+    main()
