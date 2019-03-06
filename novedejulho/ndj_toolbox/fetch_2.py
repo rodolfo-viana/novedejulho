70ac9097c402d8c2de9a18715adece9b02ea8060
Rodolfo Viana <25807981+rodolfo-viana@users.noreply.github.com>
2019-03-06 16:27:49 -0300

    Pós-revisão

--- novedejulho/ndj_toolbox/fetch_2.py
+++ novedejulho/ndj_toolbox/fetch_2.py
@@ -1,5 +1,6 @@
 import os
 import re
+from pathlib import Path
 from datetime import datetime
 import xml.etree.ElementTree as ElementTree
 from urllib.request import urlretrieve
@@ -7,15 +8,47 @@ from zipfile import ZipFile
 
 import pandas as pd
 
+TODAY = datetime.strftime(datetime.now(), '%Y-%m-%d')
+DATA_DIR = f'data_{TODAY}'
 
-class Parse_xml:
 
-    hoje = datetime.strftime(datetime.now(), '%Y-%m-%d')
-    DATA_DIR = f'data_{hoje}'
+class ParseXml:
 
-    def __init__(self, xml_data):
-        self.tree = ElementTree.parse(xml_data)
-        self.root = self.tree.getroot()
+    def __init__(self, xml_location, zip=False):
+        self.xml_location = xml_location
+
+        if not self.is_local:
+            tree = ElementTree.XML(xml_location)
+            self.root = tree.getroot()
+
+        elif self.is_zipped:
+            data = self.unzip()
+            self.root = ElementTree.XML(data)
+
+        else:
+            self.root = ElementTree.XML(xml_location)
+
+    @property
+    def is_local(self):
+        return not self.xml_location.lower().startswith('http')
+
+    @property
+    def is_zipped(self):
+        return self.xml_location.lower().endswith('.zip')
+
+    def unzip(self):
+        url_pattern = re.compile(r'http|s:.*\w*\.zip$')
+        zip_name = re.sub(url_pattern, r'\w*\.zip$', self.url)
+
+        urlretrieve(self.url, f'{self.DATA_DIR}/{zip_name}')
+        with ZipFile(f'{self.DATA_DIR}/{zip_name}', 'r') as zip_file:
+            zip_file.extractall(f'{self.DATA_DIR}')
+        os.remove(f'{self.DATA_DIR}/{zip_name}')
+
+        for filename in Path(self.DATA_DIR).glob('*.xml'):
+            self.xml_data = filename
+            break
+            return filename
 
     def parse_root(self, root=None):
         root = root if root else self.root
@@ -39,48 +72,16 @@ class Parse_xml:
         return pd.DataFrame(self.parse_root())
 
 
-class Parse_xml_external(Parse_xml):
-
-    def __init__(self, xml_data):
-        super().__init__(xml_data)
-        self.root = ElementTree.XML(xml_data)
-
-
-class Retrieve_zip(Parse_xml):
-
-    def __init__(self, url, xml_data):
-        super().__init__(xml_data)
-        self.url = url
-
-    def unzip(self):
-        url_pattern = re.compile(r'http|s:.*\w*\.zip$')
-        zip_name = re.sub(url_pattern, r'\w*\.zip$', self.url)
-        urlretrieve(self.url, f'{self.DATA_DIR}/{zip_name}')
-        zip_file = ZipFile(f'{self.DATA_DIR}/{zip_name}', 'r')
-        zip_file.extractall(f'{self.DATA_DIR}')
-        zip_file.close()
-        for file in os.listdir(f'{self.DATA_DIR}'):
-            if file.endswith('.xml'):
-                self.xml_data = file
-        os.remove(f'{self.DATA_DIR}/{zip_name}')
-
-        return self.xml_data
-
-
 def save(name):
-    '''
-    Função para salvar dataset em csv, na pasta
-    correta e com os parâmetros pré-estabelecidos
-    USO:
-        >> save('name')
-    INPUT:
-        (str) 'name': nome do arquivo
-    OUTPUT:
-        (file) 'pasta_correta/file_name.csv'
-    '''
+    """Função para salvar dataset em csv, na pasta correta e com os parâmetros
+    pré-estabelecidos:
 
-    hoje = datetime.strftime(datetime.now(), '%Y-%m-%d')
-    DATA_DIR = f'data_{hoje}'
+    >> save('name')
+    'pasta_correta/name.csv'
+
+    :param name: (str) nome do arquivo
+    :return: (str) caminho para o arquivo salvo
+    """
 
     file_name = f'{name}.csv'
 
