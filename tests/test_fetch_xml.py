efd9c68a31d6ad24cdec34e77748f619cae8ee7e
Rodolfo Viana <25807981+rodolfo-viana@users.noreply.github.com>
2019-03-06 03:06:40 -0300

    Validação de funções de tipos

--- tests/test_fetch_xml.py
+++ tests/test_fetch_xml.py
@@ -75,19 +75,18 @@ def test_type_str():
 
 
 def test_sanitize_int():
-    return int(k for k in dataset['ano'])
     for k in dataset['ano']:
         assert type(k) == int
 
 
 def test_sanitize_float():
-    return float(l for l in dataset['preco'])
     for l in dataset['preco']:
         assert type(l) == float
 
 
 def test_sum():
-    return float(m for m in dataset['preco'])
+    for m in dataset['preco']:
+        dataset['preco'].strip().replace(m, float(m), inplace=True)
     assert dataset['preco'].sum() == 270.0
 
 
