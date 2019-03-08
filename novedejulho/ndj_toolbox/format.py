4ab361c91f9615c33da9acfefa5d276c56a29d09
Rodolfo Viana <25807981+rodolfo-viana@users.noreply.github.com>
2019-03-08 19:40:08 -0300

    Aprimoramento de classes

--- novedejulho/ndj_toolbox/format.py
+++ novedejulho/ndj_toolbox/format.py
@@ -3,41 +3,6 @@ import glob
 import csv
 import sqlite3
 from datetime import datetime
-import pandas as pd
-
-
-# Utilitário para junção de dataframes em 'cand_bens.py'
-
-def merge_dfs():
-    cands = pd.read_csv(f'{DATA_DIR}/candidatos.csv')
-    dataset = cands.merge(bens, on='sq_candidato', how='left')
-    dataset['vlr_bem'].fillna(0, inplace=True)
-    sanitize_float(dataset['vlr_bem'], inplace=True)
-    dataset.drop(columns=[
-        'dt_geracao_x', 'hh_geracao_x', 'ano_eleicao_x', 'cd_tipo_eleicao_x',
-        'nm_tipo_eleicao_x', 'nr_turno', 'cd_eleicao_x', 'ds_eleicao_x',
-        'dt_eleicao_x', 'tp_abrangencia', 'sg_uf_x', 'sg_ue_x', 'nm_ue_x',
-        'cd_cargo', 'ds_cargo', 'nm_email', 'cd_situacao_candidatura', 'ds_situacao_candidatura',
-        'cd_detalhe_situacao_cand', 'ds_detalhe_situacao_cand', 'tp_agremiacao',
-        'nr_partido', 'sg_partido', 'nm_partido', 'sq_coligacao',
-        'nm_coligacao', 'ds_composicao_coligacao', 'cd_nacionalidade',
-        'ds_nacionalidade', 'sg_uf_nascimento', 'cd_municipio_nascimento',
-        'nm_municipio_nascimento', 'dt_nascimento', 'nr_idade_data_posse',
-        'nr_titulo_eleitoral_candidato', 'cd_genero', 'ds_genero',
-        'cd_grau_instrucao', 'ds_grau_instrucao', 'cd_estado_civil',
-        'ds_estado_civil', 'cd_cor_raca', 'ds_cor_raca', 'cd_ocupacao',
-        'ds_ocupacao', 'nr_despesa_max_campanha', 'cd_sit_tot_turno',
-        'ds_sit_tot_turno', 'st_reeleicao', 'st_declarar_bens',
-        'nr_protocolo_candidatura', 'nr_processo'
-    ], inplace=True)
-    dataset = dataset[[
-        'dt_geracao_y', 'hh_geracao_y', 'ano_eleicao_y', 'cd_tipo_eleicao_y',
-        'nm_tipo_eleicao_y', 'cd_eleicao_y', 'ds_eleicao_y', 'dt_eleicao_y',
-        'sg_uf_y', 'sg_ue_y', 'nm_ue_y', 'sq_candidato', 'nr_candidato', 'nm_candidato',
-        'nm_urna_candidato', 'nm_social_candidato', 'nr_cpf_candidato', 'nr_ordem_candidato',
-        'cd_tp_bem', 'ds_tp_bem', 'ds_bem', 'vlr_bem', 'dt_ultima_atualizacao',
-        'hh_ultima_atualizacao']]
-    dataset.columns = dataset.columns.str.replace('_y', '')
 
 
 # Utilitário para remover newline
