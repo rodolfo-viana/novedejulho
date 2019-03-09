c4ff8c0270b2f157f79d448b829e8cf12aef206a
Rodolfo Viana <25807981+rodolfo-viana@users.noreply.github.com>
2019-03-08 22:59:57 -0300

    Remoção de classe inútil

--- tests/test_parse_xml.py
+++ tests/test_parse_xml.py
@@ -4,34 +4,6 @@ import xml.etree.ElementTree as ElementTree
 import pandas as pd
 
 
-class ParseXml:
-
-    def __init__(self, xml_data):
-        self.tree = ElementTree.parse(xml_data)
-        self.root = self.tree.getroot()
-
-    def parse_root(self, root=None):
-        root = root if root else self.root
-        yield from (self.parse_element(child) for child in iter(root))
-
-    def parse_element(self, element, parsed=None):
-        if parsed is None:
-            parsed = dict()
-
-        new_values = {k: element.attrib.get(k) for k in element.keys()}
-        parsed.update(new_values)
-        if element.text:
-            parsed[element.tag] = element.text
-
-        for child in list(element):
-            self.parse_element(child, parsed)
-
-        return parsed
-
-    def process_data(self):
-        return pd.DataFrame(self.parse_root())
-
-
 class ParseXmlRemote():
 
     def __init__(self, xml_data):
