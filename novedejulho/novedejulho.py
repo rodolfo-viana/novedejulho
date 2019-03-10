aff0eaaf512e0462f4816058868a3b27e8e8a738
Rodolfo Viana <25807981+rodolfo-viana@users.noreply.github.com>
2019-03-10 11:29:52 -0300

    Adição de tqdm

--- novedejulho/novedejulho.py
+++ novedejulho/novedejulho.py
@@ -2,6 +2,8 @@ import os
 from time import time
 from datetime import datetime
 
+from tqdm import tqdm
+
 from ndj_toolbox.format import generate_db
 
 TODAY = datetime.strftime(datetime.now(), '%Y-%m-%d')
@@ -20,19 +22,26 @@ def retrieve_files():
 
     for f in sorted(files):
         module_name = f.replace('.py', '')
-        print(f"Fazendo o download de {module_name}...")
-        module = __import__(module_name)
-        module.main()
+        for module in tqdm(module_name, desc=f'{module_name}', ncols=100):
+            module = __import__(module_name)
+            module.main()
 
 
 if __name__ == '__main__':
     start = time()
-    print(f"Criando {DATA_DIR}...")
+    print('\nNove de Julho - v0.3\nPor Rodolfo Viana e colaboradores')
+    print('https://github.com/rodolfo-viana/novedejulho\n')
+    print(f'Criando pasta "{DATA_DIR}"')
     create_dir()
+    print('Pasta criada\nFazendo download de dados')
     retrieve_files()
-    print("Gerando o arquivo novedejulho.db...")
+    print('Dados baixados\nGerando o arquivo "novedejulho.db"')
     generate_db()
     end = time()
     hora, resto = divmod(end - start, 3600)
     minutos, segundos = divmod(resto, 60)
-    print("Finalizado em {:0>2}:{:0>2}:{:05.2f}.".format(int(hora), int(minutos), segundos))
+    print('Arquivo gerado')
+    print('Execução concluída em {:0>2}:{:0>2}:{:05.2f}.'.format(int(hora),
+                                                                 int(minutos),
+                                                                 segundos)
+          )
